6ad61ad6ec70717d5da40406bf8d68a27f5753f6
FlamingoWinter <davidmac1239@gmail.com>
2023-02-11 16:19:09 +0000

    added more garbage

--- Assets/BeatPositions.cs
+++ Assets/BeatPositions.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
-[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SpawnManagerScriptableObject", order = 1)]
+[CreateAssetMenu(fileName = "Data", menuName = "BeatPositions", order = 1)]
 public class BeatPositions : ScriptableObject
 {
     public List<float> beatPositions;
