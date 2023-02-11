b85c9912c2c0f89c737e0ed231178f0e5c145483
FlamingoWinter <davidmac1239@gmail.com>
2023-02-11 14:35:36 +0000

    BLahBlahBlah

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 public class Player : MoveableEntity
 {
     private PlayerStates PlayerState = PlayerStates.Idle;
+    private Animator PlayerAnimator;
     public enum PlayerStates
         {
         Moving,
         Idle
     }
+    void Start(){
+        PlayerAnimator=GetComponent<Animator>();
+    }
+    public enum Directions{
+        left,
+        right
+    }
+    private Directions DirectionFacing = Directions.right;
 
     public new void Init()
     {
@@ -20,7 +29,20 @@ public class Player : MoveableEntity
     void Update()
     {
         base.MoveableEntityUpdate();
+        UpdateLeftRight();
+    }
 
+    void UpdateLeftRight(){
+        float xScale=transform.localScale.x;
+        if (xScale>0&&DirectionFacing==Directions.left){
+            //Should be facing left 
+            xScale*=-1;
+        }
+        else if(xScale<0&&DirectionFacing==Directions.right){
+            //Should be facing right 
+            xScale*=-1;
+        }
+        transform.localScale=new Vector3(xScale,transform.localScale.y,transform.localScale.z);
     }
 
 
@@ -36,9 +58,11 @@ public class Player : MoveableEntity
                 MoveInputted(0, -1);
                 break;
             case InputManager.Actions.left:
+                DirectionFacing=Directions.left;
                 MoveInputted(-1,0);
                 break;
             case InputManager.Actions.right:
+                DirectionFacing=Directions.right;
                 MoveInputted(1,0);
                 break;
         }
@@ -47,6 +71,11 @@ public class Player : MoveableEntity
     public void MoveInputted(int deltaX, int deltaY)
     {
         bool Success = TryScheduleMove(x+deltaX,y+deltaY);
+        if(Success){
+            PlayerAnimator.Play("PlayerWalk");
+            }
+
+
         if(!Success)
         {
             Debug.Log("Move Failed");
