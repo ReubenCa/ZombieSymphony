c173918542af97f5b9c3394dc5e42745184778fb
Reuben Carolan <61869273+ReubenCa@users.noreply.github.com>
2023-02-12 16:18:18 +0000

    Everyting

--- Assets/Scripts/Heart.cs
+++ Assets/Scripts/Heart.cs
@@ -16,8 +16,15 @@ public class Heart : MonoBehaviour
     float Fill;
 
     [SerializeField]
+    ParticleSystem particles;
+    [SerializeField]
     private GameObject mask;
 
+    private void Start()
+    {
+        particles.Stop();
+    }
+    bool HasSomeHealth = true;
     public void UpdateHealth(float health)
     {
        // Debug.Log(mask.gameObject.transform.position);
@@ -25,6 +32,12 @@ public class Heart : MonoBehaviour
         mask.gameObject.transform.localPosition = new Vector3(mask.gameObject.transform.localPosition.x
             , Height, mask.gameObject.transform.localPosition.z);
       //  Debug.Log(mask.gameObject.transform.position);
+
+        if(HasSomeHealth && health <=0f)
+        {
+            particles.Play();
+        }
+        HasSomeHealth = health > 0f;
     }
 
 }
